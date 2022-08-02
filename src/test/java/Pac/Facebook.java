f125de90f57beb342229f8c6ee5db338116c2ec7
Madhavi252 <yammanuru.madhavi@gmail.com>
2022-08-02 12:28:03 +0530

    newpro

---
+++ src/test/java/Pac/Facebook.java
@@ -0,0 +1,22 @@
+package Pac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Facebook {
+	
+	@Test
+	public void faceBook() throws Throwable {
+		WebDriverManager.chromedriver().setup();
+		Thread.sleep(2000);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		System.out.println(driver.getTitle());
+		
+	}
+
+}
