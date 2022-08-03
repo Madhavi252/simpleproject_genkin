377233741cd304662c5e5a209554322b627f8224
Madhavi252 <yammanuru.madhavi@gmail.com>
2022-08-03 17:15:12 +0530

    msg

---
+++ src/test/java/Pac/Amazon.java
@@ -0,0 +1,23 @@
+package Pac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Amazon {
+
+	@Test
+	public void faceBook() throws Throwable {
+		WebDriverManager.chromedriver().setup();
+		Thread.sleep(2000);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		
+		System.out.println(driver.getTitle());
+		
+	}
+
+
+}
